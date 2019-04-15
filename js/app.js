b6b5cc855b82a6edacf35df8ec2a26e3b46b3404
Tech-a28 <tech-a28@tech-a28s-air.home>
2019-04-15 20:14:29 +0100

    added loops/error checking

--- js/app.js
+++ js/app.js
@@ -1,62 +1,94 @@
-var operand, input1, input2, answer, validChoice;
+var operand, input1, input2, answer;
 
-var choice = prompt("Would you like to use (1)basic functions or (2)advanced functions?");
+var quit = false;
+var validFunc = false;
 
-  // basic functions
-  if (choice == 1)
+// will run this loop until the user wants to quit
+do
+{
+  // will continue this loop until a valid function has been completed
+  while (!validFunc)
   {
-    operand = prompt("Do you want to do (1)addition, (2)subtraction, (3)multiplication or (4)division?");
-    switch (operand)
+    var choice = prompt("Would you like to use (1)basic functions or (2)advanced functions?");
+    if (choice == 1)
     {
-      case "1":
-        input1 = parseInt(prompt("Enter your first value"));
-        input2 = parseInt(prompt("Enter your second value"));
-        answer = input1 + input2;
-        alert(`The answer to your equation is ${answer}`);
-        break;
-      case "2":
-        input1 = parseInt(prompt("Enter your first value"));
-        input2 = parseInt(prompt("Enter your second value"));
-        answer = input1 - input2;
-        alert(`The answer to your equation is ${answer}`);
-        break;
-      case "3":
-        input1 = parseInt(prompt("Enter your first value"));
-        input2 = parseInt(prompt("Enter your second value"));
-        answer = input1 * input2;
-        alert(`The answer to your equation is ${answer}`);
-        break;
-      case "4":
-        input1 = parseInt(prompt("Enter your first value"));
-        input2 = parseInt(prompt("Enter your second value"));
-        answer = input1 / input2;
-        alert(`The answer to your equation is ${answer}`);
-        break;
-      default:
-        alert("You did not choose a valid operation so we have gone with addition");
-        input1 = parseInt(prompt("Enter your first value"));
-        input2 = parseInt(prompt("Enter your second value"));
-        answer = input1 + input2;
-        alert(`The answer to your equation is ${answer}`);
+      operand = prompt("Do you want to do (1)addition, (2)subtraction, (3)multiplication or (4)division?");
+      switch (operand)
+      {
+        case "1":
+          input1 = parseInt(prompt("Enter your first value"));
+          input2 = parseInt(prompt("Enter your second value"));
+          answer = input1 + input2;
+          alert(`The answer to your equation is ${answer}`);
+          validFunc = true;
+          break;
+        case "2":
+          input1 = parseInt(prompt("Enter your first value"));
+          input2 = parseInt(prompt("Enter your second value"));
+          answer = input1 - input2;
+          alert(`The answer to your equation is ${answer}`);
+          validFunc = true;
+          break;
+        case "3":
+          input1 = parseInt(prompt("Enter your first value"));
+          input2 = parseInt(prompt("Enter your second value"));
+          answer = input1 * input2;
+          alert(`The answer to your equation is ${answer}`);
+          validFunc = true;
+          break;
+        case "4":
+          input1 = parseInt(prompt("Enter your first value"));
+          input2 = parseInt(prompt("Enter your second value"));
+          answer = input1 / input2;
+          alert(`The answer to your equation is ${answer}`);
+          validFunc = true;
+          break;
+        default:
+          alert("You did not choose a valid operation so we have gone with addition");
+          input1 = parseInt(prompt("Enter your first value"));
+          input2 = parseInt(prompt("Enter your second value"));
+          answer = input1 + input2;
+          alert(`The answer to your equation is ${answer}`);
+          validFunc = true;
+      }
+    }
+    // more advanced functions
+    else if (choice == 2)
+    {
+      operand = prompt("Do you want to calulate (1)a number to the power of n or (2) the square root of a number?");
+      switch (operand)
+      {
+        case "1":
+          input1 = parseInt(prompt("Enter your first value"));
+          input2 = parseInt(prompt("Enter your second value"));
+          answer = Math.pow(input1, input2);
+          alert(`The answer to your equation is ${answer}`);
+          validFunc = true;
+          break;
+        case "2":
+          input1 = parseInt(prompt("Enter the number you want to square root"));
+          answer = Math.sqrt(input1);
+          alert(`The answer to your equation is ${answer}`);
+          validFunc = true;
+          break;
+        default:
+          alert("You did not choose a valid option so we have gone with sqaure root");
+          input1 = parseInt(prompt("Enter the number you want to square root"));
+          answer = Math.sqrt(input1);
+          alert(`The answer to your equation is ${answer}`);
+          validFunc = true;
+      }
+    }
+    else
+    {
+      alert("That is not a valid option!");
     }
   }
-  // more advanced functions
-  else if (choice == 2)
-  {
-    operand = prompt("Do you want to calulate (1)a number to the power of n or (2) the square root of a number?");
-    switch (operand) {
-      case "1":
-        input1 = parseInt(prompt("Enter your first value"));
-        input2 = parseInt(prompt("Enter your second value"));
-        answer = Math.pow(input1, input2);
-        alert(`The answer to your equation is ${answer}`);
-        break;
-      case "2":
-        input1 = parseInt(prompt("Enter the number you want to square root"));
-        answer = Math.sqrt(input1);
-        alert(`The answer to your equation is ${answer}`);
-        break;
-      default:
 
-    }
+  var repeat = prompt("Do you want to do another calculation? Yes/No?");
+  if (repeat === "No" || "no")
+  {
+    quit = true;
   }
+
+} while (quit === false);
