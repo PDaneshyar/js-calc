98249b17cd794477648cd60469bbdb901cfdef9e
Tech-a28 <tech-a28@tech-a28s-air.home>
2019-04-15 19:37:30 +0100

    added advanced funtions

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ var operand, input1, input2, answer, validChoice;
 
 var choice = prompt("Would you like to use (1)basic functions or (2)advanced functions?");
 
-
-  if (1)
+  // basic functions
+  if (choice == 1)
   {
     operand = prompt("Do you want to do (1)addition, (2)subtraction, (3)multiplication or (4)division?");
     switch (operand)
@@ -40,8 +40,23 @@ var choice = prompt("Would you like to use (1)basic functions or (2)advanced fun
         alert(`The answer to your equation is ${answer}`);
     }
   }
-  else if (2)
+  // more advanced functions
+  else if (choice == 2)
   {
     operand = prompt("Do you want to calulate (1)a number to the power of n or (2) the square root of a number?");
-    
+    switch (operand) {
+      case "1":
+        input1 = parseInt(prompt("Enter your first value"));
+        input2 = parseInt(prompt("Enter your second value"));
+        answer = Math.pow(input1, input2);
+        alert(`The answer to your equation is ${answer}`);
+        break;
+      case "2":
+        input1 = parseInt(prompt("Enter the number you want to square root"));
+        answer = Math.sqrt(input1);
+        alert(`The answer to your equation is ${answer}`);
+        break;
+      default:
+
+    }
   }
