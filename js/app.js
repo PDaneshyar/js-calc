ab55065dd4ac7c540de8d4fdab816097e0a9823a
Tech-a28 <tech-a28@Tech-A28s-MacBook-Air.local>
2019-04-16 10:06:45 +0100

    added default value to inputs

--- js/app.js
+++ js/app.js
@@ -13,8 +13,8 @@ do
     if (choice == 1)
     {
       operand = prompt("Do you want to do (1)addition, (2)subtraction, (3)multiplication or (4)division?");
-      input1 = parseFloat(prompt("Enter your first value"));
-      input2 = parseFloat(prompt("Enter your second value"));
+      input1 = parseFloat(prompt("Enter your first value")) || 0;
+      input2 = parseFloat(prompt("Enter your second value")) || 0;
 
       switch (operand)
       {
@@ -52,21 +52,21 @@ do
       switch (operand)
       {
         case "1":
-          input1 = parseFloat(prompt("Enter your first value"));
-          input2 = parseFloat(prompt("Enter your second value"));
+          input1 = parseFloat(prompt("Enter your first value")) || 0;
+          input2 = parseFloat(prompt("Enter your second value")) || 0;
           answer = Math.pow(input1, input2);
           alert(`The answer to your equation is ${answer}`);
           validFunc = true;
           break;
         case "2":
-          input1 = parseFloat(prompt("Enter the number you want to square root"));
+          input1 = parseFloat(prompt("Enter the number you want to square root")) || 0;
           answer = Math.sqrt(input1);
           alert(`The answer to your equation is ${answer}`);
           validFunc = true;
           break;
         default:
           alert("You did not choose a valid option so we have gone with sqaure root");
-          input1 = parseFloat(prompt("Enter the number you want to square root"));
+          input1 = parseFloat(prompt("Enter the number you want to square root")) || 0;
           answer = Math.sqrt(input1);
           alert(`The answer to your equation is ${answer}`);
           validFunc = true;
